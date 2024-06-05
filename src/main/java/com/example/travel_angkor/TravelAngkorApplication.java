c0541a4b995ccb905c4152c24fea7fe1394e0509
kimlong <104357626+Jinlong77@users.noreply.github.com>
2024-06-05 12:53:22 +0700

    init

---
+++ src/main/java/com/example/travel_angkor/TravelAngkorApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_angkor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAngkorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAngkorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel_angkor/TravelAngkorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_angkor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAngkorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
